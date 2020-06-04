20a5a7dd5a3fa6af7a6733a8fa680a4a22f5bd0a
addempsea <babatundeademola4@gmail.com>
2020-06-04 21:32:57 +0100

    fix(contact): drop and recreate table

--- app/db/migrations/sqls/20200604101840-alter-some-tables-up.sql
+++ app/db/migrations/sqls/20200604101840-alter-some-tables-up.sql
@@ -3,4 +3,17 @@
 ALTER TABLE IF EXISTS user_info ADD COLUMN facility_id uuid REFERENCES facility(id) NOT NULL;
 ALTER TABLE IF EXISTS user_info RENAME COLUMN user_type TO role;
 ALTER TABLE IF EXISTS facility ADD COLUMN address VARCHAR(150);
-ALTER TABLE IF EXISTS contact_us ALTER COLUMN facility_type TYPE facility_facility_type USING facility_type::facility_facility_type;
+DROP TABLE IF EXISTS contact_us;
+
+CREATE TABLE IF NOT EXISTS contact_us(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  company_name VARCHAR(100) NOT NULL,
+  email VARCHAR(150)  NOT NULL,
+  phone_number VARCHAR(30) NOT NULL,
+  message TEXT NOT NULL,
+  number_of_tanks INTEGER NOT NULL,
+  facility_type facility_facility_type NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
