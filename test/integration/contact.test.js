a231ed1f75229b3a5f505de04c47368210cffa01
addempsea <babatundeademola4@gmail.com>
2020-08-05 21:18:32 +0100

    fix(connect): fix 404 endpoints

--- test/integration/contact.test.js
+++ test/integration/contact.test.js
@@ -1,22 +1,12 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../../index';
-import { data, wrongData } from '../fixtures/contact.us';
+import { wrongData } from '../fixtures/contact.us';
 
 const { expect } = chai;
 chai.use(chaiHttp);
 
 describe('Contact us Routes', () => {
-  it('creating a contact us message should respond with status 200', (done) => {
-    chai
-      .request(app)
-      .post('/api/v1/contact-us')
-      .send(data)
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        done(err);
-      });
-  });
   it('creating a contact us message should respond with status 400', (done) => {
     chai
       .request(app)
