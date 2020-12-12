6bba6b62a3aeb625d719480030232fc1af629acc
addempsea <babatundeademola4@gmail.com>
2020-12-12 14:42:12 +0100

    fix(company-validation): add validation to company schema

--- app/validations/device.js
+++ app/validations/device.js
@@ -1,10 +1,11 @@
 import Joi from 'joi';
+import { stringCheck, editStringCheck } from './generic';
 
-const deviceSchema = Joi.object().keys({
-  companyId: Joi.string(),
-  facilityId: Joi.string(),
-  tankId: Joi.string(),
-  serialNumber: Joi.string().required(),
+const deviceSchema = Joi.object({
+  companyId: editStringCheck('Company id', Joi),
+  facilityId: editStringCheck('Facility id', Joi),
+  tankId: editStringCheck('Tank id', Joi),
+  serialNumber: stringCheck('Serial number', Joi),
   distToDevice: Joi.number().required()
 });
 
--- app/validations/facility.js
+++ app/validations/facility.js
@@ -1,13 +1,14 @@
 import Joi from 'joi';
+import { stringCheck, numberCheck, editStringCheck } from './generic';
 
-const facilitySchema = Joi.object().keys({
-  companyId: Joi.string(),
-  gpsCoordinate: Joi.string().required(),
-  facilityName: Joi.string().min(4).required(),
-  facilityType: Joi.string().required(),
-  address: Joi.string().required(),
-  closingTime: Joi.number(),
-  openingTime: Joi.number()
+const facilitySchema = Joi.object({
+  companyId: editStringCheck('Company id', Joi),
+  gpsCoordinate: stringCheck('GPS coordinates', Joi),
+  facilityName: stringCheck('Facility Name ', Joi, 4),
+  facilityType: stringCheck('Facility type', Joi),
+  address: stringCheck('Address ', Joi),
+  closingTime: numberCheck('Closing time', Joi),
+  openingTime: numberCheck('Opening time', Joi)
 });
 
 export default facilitySchema;
